ec41c1bfd998fcaaeac97abadf9344850c2096b2
Jan <jahoo1990@gmail.com>
2026-03-23 15:03:27 +0100

    CRUD is done

---
+++ src/users/users.service.ts
@@ -0,0 +1,57 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity/user.entity';
+import { CreateUserDto } from './dto/create-user.dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+  findAll() {
+    return this.usersRepository.find();
+  }
+
+  async findOne(id: number) {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const result = await this.usersRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException('User not found');
+    }
+  }
+
+  async update(id: number, upadteUserDto: UpdateUserDto) {
+    const user = await this.usersRepository.preload({
+      id,
+      ...upadteUserDto,
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.usersRepository.save(user);
+  }
+}
